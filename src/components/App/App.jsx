36ed59e33f7e382a9ab3e221705fd1d84a31331d
santiagogustavo <santiagogustavo@gmail.com>
2019-01-06 04:11:18 +0000

    :sparkles: Add eslint with core settings, fix errors

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from 'assets/imgs/logo.svg';
+import 'assets/stylesheets/App.css';
+
+const App = () => (
+  <div className="App">
+    <div id="video-placeholder" />
+    <script src="https://www.youtube.com/iframe_api" />
+  </div>
+);
+
+export default App;
